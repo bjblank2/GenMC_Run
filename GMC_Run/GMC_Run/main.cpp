d55fd08df49b3e4723148730233d48581029c297
Tianyu <tianyus5@illinois.edu>
2023-08-29 15:25:13 -0500

    takes command line arguments as input parameters

--- GMC_Run/GMC_Run/main.cpp
+++ GMC_Run/GMC_Run/main.cpp
@@ -9,7 +9,17 @@ using namespace std;
 // The paramiters for the MC run ( Composition, Simulation cell size, ect...) are all set in INPUT.txt
 // These settings are read in and stored in the SimCell object. The SimCell object is then reffrenced to set up the actual MC run.
 SimCell sim_cell; // The simulation cell object. This object contains all the information about the simulation cell and the atoms in it.
-int main(string input, string poscar, string spin_states) {
+int main(int argc, char** argv) {
+    string input = "INPUT";
+    string poscar = "POSCAR";
+    string spin_states = "SPIN_STATES";
+    vector<string> setting;
+    for (int i = 1; i < argc; i++) {
+        setting = split(argv[i], "=");
+        if (setting[0].compare("-I") == 0) { input = setting[1]; }
+        if (setting[0].compare("-P") == 0) { poscar = setting[1]; }
+        if (setting[0].compare("-S") == 0) { spin_states = setting[1]; }
+    }
 	Session ses(input); // settings for MC run from input file
 	if (ses.use_states == true) { ses.add_spin_states(spin_states); }
 	ses.fill_rule_list(); // Initalize the "Rules" that will be used to properly apply the ECI to each site in the simulation cell.
